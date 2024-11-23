2317d4e273120ebdc4d0890f5cfd3a62f56147d1
[markis220179] <[m_ariel1979@hotmail.com]>
2024-11-23 19:34:55 -0300

    Practico get y post

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 import express from 'express'
+import { films } from './filmsData.js'
+
 const app = express()
 const port = 3000
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.json(films)
+})
+
+app.post('/', (req, res) => {
+  const newFilm = req.body;
+  films.push(newFilm);
+  res.send('Nueva pelicula agregada')
+})
+
+app.put('/user', (req, res) => {
+  res.send('Got a PUT request at /user')
+})
+
+app.delete('/user', (req, res) => {
+  res.send('Got a DELETE request at /user')
 })
 
 app.listen(port, () => {
---
+++ filmsData.js
@@ -0,0 +1,25 @@
+export const films = [
+    {
+      id: 1,
+      titulo: "Inception",
+      director: "Christopher Nolan",
+      anio_estreno: 2010,
+      genero: "Ciencia ficción"
+    },
+    {
+      id: 2,
+      titulo: "The Godfather",
+      director: "Francis Ford Coppola",
+      anio_estreno: 1972,
+      genero: "Crimen"
+    },
+    {
+      id: 3,
+      titulo: "Pulp Fiction",
+      director: "Quentin Tarantino",
+      anio_estreno: 1994,
+      genero: "Drama"
+    }
+];
+
+  
