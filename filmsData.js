63a5e076f8ea9f496895614708ce48b479740f32
[markis220179] <[m_ariel1979@hotmail.com]>
2024-11-24 22:15:01 -0300

    Actualizo los datos y uso postman

--- filmsData.js
+++ filmsData.js
@@ -1,25 +1,26 @@
 export const films = [
-    {
-      id: 1,
-      titulo: "Inception",
-      director: "Christopher Nolan",
-      anio_estreno: 2010,
-      genero: "Ciencia ficción"
-    },
-    {
-      id: 2,
-      titulo: "The Godfather",
-      director: "Francis Ford Coppola",
-      anio_estreno: 1972,
-      genero: "Crimen"
-    },
-    {
-      id: 3,
-      titulo: "Pulp Fiction",
-      director: "Quentin Tarantino",
-      anio_estreno: 1994,
-      genero: "Drama"
-    }
+  {
+    id: 1,
+    titulo: "El Origen",
+    director: "Christopher Nolan",
+    anio_estreno: 2010,
+    genero: "Ciencia ficción",
+    img: "https://m.media-amazon.com/images/I/81p+xe8cbnL._AC_SY679_.jpg"
+  },
+  {
+    id: 2,
+    titulo: "El Padrino",
+    director: "Francis Ford Coppola",
+    anio_estreno: 1972,
+    genero: "Crimen",
+    img: "https://m.media-amazon.com/images/I/71x0lCDEgSL._AC_SY679_.jpg"
+  },
+  {
+    id: 3,
+    titulo: "Tiempos Violentos",
+    director: "Quentin Tarantino",
+    anio_estreno: 1994,
+    genero: "Drama",
+    img: "https://m.media-amazon.com/images/I/61o-r1J2XrL._AC_SY679_.jpg"
+  },
 ];
-
-  
